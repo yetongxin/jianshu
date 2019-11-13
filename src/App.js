5753c5be4d6e0eb0c8fe841a2c9d7dcbc9c5f1f1
yetongxin <yetxszu@gmail.com>
2019-11-13 23:16:51 +0800

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './common/header'
 import { Provider } from 'react-redux';
 import { GlobalIconStyle } from './static/iconfont/iconfont.js'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/home'
 import store from './store'
 import Detail from './pages/detail'
@@ -11,13 +11,13 @@ import Detail from './pages/detail'
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <Header />
         <GlobalIconStyle />
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/detail/:id" component={Detail} />
-      </BrowserRouter>
+      </Router>
       
     </Provider>
   );
